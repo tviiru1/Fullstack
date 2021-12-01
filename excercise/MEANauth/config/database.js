91792a231dcc17dafa24858fac90d9586dc0e106
tviiru1 <tero.viiru@kolumbus.fi>
2021-12-01 12:33:15 +0200

    Add files via upload

---
+++ excercise/MEANauth/config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  //database: 'mongodb://tviiru:tviiru@ds12226.mlab.com:12226/meanauthapp',   //prod
+  database: 'mongodb://localhost:27017/meanauth',    //dev
+  //database: 'mongodb+srv://tviiru:tviiru@cluster0.jyez7.mongodb.net/test',
+  secret: 'yoursecret'
+}
